e332a2e8e952f282d03f3b0601dc69ab947a0142
unknown <umernk42@gmail.com>
2022-12-16 10:16:54 +0500

    Added Content

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function toggleNav(){
     const outerGrid = document.querySelector('.outer-grid');
     outerGrid.classList.toggle('show-nav');
+
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('hide-nav');
 }
