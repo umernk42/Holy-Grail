44d1f29bde0584d168c6074e93ad8856b13cd74f
unknown <umernk42@gmail.com>
2023-03-20 15:22:54 +0500

    'Some_improvements'

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 function toggleNav(){
-    const outerGrid = document.querySelector('.outer-grid');
+    const outerGrid = document.querySelector('.inner-grid');
     outerGrid.classList.toggle('show-nav');
 
     const nav = document.querySelector('nav');
