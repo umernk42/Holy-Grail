a44755c0de6c18f15afaf6392bbb13812b67c0dd
unknown <umernk42@gmail.com>
2023-03-20 12:05:35 +0500

    'nav_bug_fixed'

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function toggleNav(){
     const outerGrid = document.querySelector('.outer-grid');
     outerGrid.classList.toggle('show-nav');
@@ -5,3 +7,18 @@ function toggleNav(){
     const nav = document.querySelector('nav');
     nav.classList.toggle('hide-nav');
 }
+
+const nav = document.querySelector('nav');
+
+if(window.innerWidth > 576){
+    nav.classList.remove('hide-nav');
+}
+
+window.addEventListener('resize',(e) => {
+    if(window.innerWidth > 576){
+        nav.classList.remove('hide-nav');
+    }
+    else{
+        nav.classList.add('hide-nav');
+    }
+});
